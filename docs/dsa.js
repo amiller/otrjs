c99e1ffe230e517bec3bbcaff9448db521aa99a4
Andrew Miller <amiller@dappervision.com>
2011-07-25 06:19:07 -0400

    Moved to using pre-computed parameters (e.g. RFC2412 Group 2). Fast generation
    Signed-off-by: Andrew Miller <amiller@dappervision.com>

--- docs/dsa.js
+++ docs/dsa.js
@@ -12,11 +12,11 @@
 
 // "empty" DSA key constructor
 function DSAKey() {
-  this.p = null;
-  this.q = null;
-  this.g = null;
-  this.y = null;
-  this.x = null;
+    this.p = null;
+    this.q = null;
+    this.g = null;
+    this.y = null;
+    this.x = null;
 }
 
 // Set the private key fields p, q, g, y from hex strings
@@ -36,38 +36,66 @@ function DSASetPublic(p, q, g, y) {
     this.y = new BigInteger(y,16);
 }
 
-function generateQ(rng, H) {
-    // H must return a byte array, NOT a hex digest!
-    var S = new BigInteger(160,rng);
-    var hash1 = H(S.toString(16));
-    var hash2 = H(S.add(BigInteger.ONE).toString(16));
-    if (typeof(hash1) != "object")
-	throw "Hash function H must return a byte array, not " + typeof(hash1);
-    if (typeof(hash2) != "object")
-	throw "Hash function H must return a byte array, not " + typeof(hash2);
-    var q = BigInteger.ZERO;
-    for (var i = 0; i < 20; i++) {
-	var c = hash1[i] ^ hash2[i];
-	if (i ==  0) c |= 128;
-	if (i == 19) c |= 1;
-	//q = q.shiftLeft(8);
-	q = q.multiply(new BigInteger('256'));
-	q = q.add(new BigInteger(String(c)));
+
+// Generate x and y, assuming parameters p, q, g set previously
+function DSAGeneratePrivate() {
+    var rng = new SecureRandom();
+    while (1) {
+	var x = new BigInteger(160, rng);
+	if (BigInteger.ZERO.compareTo(x) < 0 &&
+	    x.compareTo(this.q) < 0)
+	    break;
     }
-    // The JSBN implementation of this uses Rabin-Miller to find strong
-    // probable primes
-    var TWO = new BigInteger('2');
-    while (!q.isProbablePrime(50)) {
-	q = q.add(TWO);
-    }
-    //console.info([TWO.pow(159).compareTo(q), TWO.pow(160).compareTo(q)
-    if (TWO.pow(159).compareTo(q) < 0 && TWO.pow(160).compareTo(q) > 0)
-	return [S, q];
-    throw 'Bad q value generated';
+    this.x = x;
+    this.y = this.g.modPow(this.x, this.p);
 }
 
-// Generate a new random private Parkey B bits long, using hash function H
-function DSAGenerate(B, H) {
+// Sign a message digest (using random key K)
+function DSASign(Hm, K) {
+    if (K == undefined) {
+	var rng = new SecureRandom();
+	K = new BigInteger(19, rng).add(new BigInteger('2'));
+    }
+    if (K.compareTo(new BigInteger('2')) < 0 ||
+	this.q.compareTo(K) <= 0) {
+	throw 'K is not between 2 and q';
+    }
+    var r = this.g.modPow(K, this.p).mod(this.q);
+    var k1 = K.modInverse(this.q)
+    var s = k1.multiply(Hm.add(this.x.multiply(r))).mod(this.q);
+    return [r,s];
+}
+
+function DSAVerify(Hm, Sig) {
+    var r = Sig[0];
+    var s = Sig[1];
+    if (r.compareTo(BigInteger.ZERO) <= 0 || 
+	r.compareTo(this.q) >= 0 || 
+	s.compareTo(BigInteger.ZERO) <= 0 || 
+	s.compareTo(this.q) >= 0) {
+	return 0;
+    }
+    var w = s.modInverse(this.q);
+    var u1 = Hm.multiply(w).mod(this.q);
+    var u2 = r.multiply(w).mod(this.q);
+    var v1 = this.g.modPow(u1, this.p);
+    var v2 = this.y.modPow(u2, this.p);
+    var v = v1.multiply(v2).mod(this.p).mod(this.q);
+    return v.equals(r);
+}
+
+function DSADump() {
+    return {
+	'p': this.p.toString(16),
+	'q': this.q.toString(16),
+	'g': this.g.toString(16),
+	'y': this.y.toString(16),
+	'x': this.x.toString(16)
+    }	
+}
+
+function DSAGenerateParameters(B, H) {
+    // Generate p,q,g
     if (B < 160) throw 'Key length < 160 bits: ' + B;
     var rng = new SecureRandom();
     var TWO = new BigInteger('2');
@@ -111,61 +139,72 @@ function DSAGenerate(B, H) {
 	    g.compareTo(BigInteger.ONE) > 0)
 	    break;
     }
-    this.g = g;
-    console.info('g^q mod p: ' + this.g.modPow(this.q, this.p).toString());
-    //console.info('x,y');
-    while (1) {
-	var x = new BigInteger(160,rng);
-	if (BigInteger.ZERO.compareTo(x) < 0 && 
-	    x.compareTo(this.q) < 0)
-	    break;
-    }
-    this.x = x;
-    this.y = this.g.modPow(this.x, this.p);
 }
 
-function DSASign(Hm, K) {
-    if (K.compareTo(new BigInteger('2')) < 0 || 
-	this.q.compareTo(K) <= 0) {
-	throw 'K is not between 2 and q';
+function generateQ(rng, H) {
+    // H must return a byte array, NOT a hex digest!
+    var S = new BigInteger(160,rng);
+    var hash1 = H(S.toString(16));
+    var hash2 = H(S.add(BigInteger.ONE).toString(16));
+    if (typeof(hash1) != "object")
+	throw "Hash function H must return a byte array, not " + typeof(hash1);
+    if (typeof(hash2) != "object")
+	throw "Hash function H must return a byte array, not " + typeof(hash2);
+    var q = BigInteger.ZERO;
+    for (var i = 0; i < 20; i++) {
+	var c = hash1[i] ^ hash2[i];
+	if (i ==  0) c |= 128;
+	if (i == 19) c |= 1;
+	//q = q.shiftLeft(8);
+	q = q.multiply(new BigInteger('256'));
+	q = q.add(new BigInteger(String(c)));
     }
-    var r = this.g.modPow(K, this.p).mod(this.q);
-    var k1 = K.modInverse(this.q)
-    var s = k1.multiply(Hm.add(this.x.multiply(r))).mod(this.q);
-    return [r,s];
+    // The JSBN implementation of this uses Rabin-Miller to find strong
+    // probable primes
+    var TWO = new BigInteger('2');
+    while (!q.isProbablePrime(50)) {
+	q = q.add(TWO);
+    }
+    //console.info([TWO.pow(159).compareTo(q), TWO.pow(160).compareTo(q)
+    if (TWO.pow(159).compareTo(q) < 0 && TWO.pow(160).compareTo(q) > 0)
+	return [S, q];
+    throw 'Bad q value generated';
 }
 
-function DSAVerify(Hm, Sig) {
-    var r = Sig[0];
-    var s = Sig[1];
-    if (r.compareTo(BigInteger.ZERO) <= 0 || 
-	r.compareTo(this.q) >= 0 || 
-	s.compareTo(BigInteger.ZERO) <= 0 || 
-	s.compareTo(this.q) >= 0) {
-	return 0;
-    }
-    var w = s.modInverse(this.q);
-    var u1 = Hm.multiply(w).mod(this.q);
-    var u2 = r.multiply(w).mod(this.q);
-    var v1 = this.g.modPow(u1, this.p);
-    var v2 = this.y.modPow(u2, this.p);
-    var v = v1.multiply(v2).mod(this.p).mod(this.q);
-    return v.equals(r);
+function DSAGroup2() {
+    var p = "\
+    FFFFFFFF FFFFFFFF C90FDAA2 2168C234 C4C6628B 80DC1CD1\
+    29024E08 8A67CC74 020BBEA6 3B139B22 514A0879 8E3404DD\
+    EF9519B3 CD3A431B 302B0A6D F25F1437 4FE1356D 6D51C245\
+    E485B576 625E7EC6 F44C42E9 A637ED6B 0BFF5CB6 F406B7ED\
+    EE386BFB 5A899FA5 AE9F2411 7C4B1FE6 49286651 ECE65381\
+    FFFFFFFF FFFFFFFF";
+    var q = "\
+    7FFFFFFF FFFFFFFF E487ED51 10B4611A 62633145 C06E0E68\
+    94812704 4533E63A 0105DF53 1D89CD91 28A5043C C71A026E\
+    F7CA8CD9 E69D218D 98158536 F92F8A1B A7F09AB6 B6A8E122\
+    F242DABB 312F3F63 7A262174 D31BF6B5 85FFAE5B 7A035BF6\
+    F71C35FD AD44CFD2 D74F9208 BE258FF3 24943328 F67329C0\
+    FFFFFFFF FFFFFFFF";
+    this.p = new BigInteger(p, 16);
+    this.q = new BigInteger(q, 16);
+    this.g = new BigInteger('2');
+    this.generatePrivate()
 }
 
-function DSADump() {
-    return {
-	'p': this.p.toString(16),
-	'q': this.q.toString(16),
-	'g': this.g.toString(16),
-	'y': this.y.toString(16),
-	'x': this.x.toString(16)
-    }	
+function DSAValidateKey() {
+    // FIXME Finish key validation here? Add other validation
+    if (this.g.modPow(this.q, this.p) != 1)
+	throw 'g^q != 1 (modp)';
+    return true;
 }
 
 DSAKey.prototype.sign = DSASign;
 DSAKey.prototype.verify = DSAVerify;
 DSAKey.prototype.setPrivate = DSASetPrivate;
 DSAKey.prototype.setPublic = DSASetPublic;
-DSAKey.prototype.generate = DSAGenerate;
+DSAKey.prototype.generateParameters = DSAGenerateParameters;
+DSAKey.prototype.generatePrivate = DSAGeneratePrivate;
+DSAKey.prototype.validateKey = DSAValidateKey;
+DSAKey.prototype.group2 = DSAGroup2;
 DSAKey.prototype.dump = DSADump;
