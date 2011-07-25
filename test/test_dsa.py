42af9f8318adbea32036112ca835a4d656221f74
Andrew Miller <amiller@dappervision.com>
2011-07-25 15:35:57 -0400

    Reorganized a bit
    Signed-off-by: Andrew Miller <amiller@dappervision.com>

--- test/test_dsa.py
+++ test/test_dsa.py
@@ -46,6 +46,50 @@ def test_key():
     assert(key.verify(H.digest(), signature))
 
 
+def validate_key(key):
+    assert pow(key.g, key.q, key.p) == 1
+
+
+def generateDSA(p, q, g):
+    power = (p-1)/q
+    while (1):
+        h = 2
+        g = pow(h, power, p)
+        x = bytes_to_long(os.urandom(20))
+        if 0 < x < q:
+            break
+    y = pow(g, x, p)
+    key = DSA.construct((y, g, p, q, x))
+    return key
+
+
+def test_group2():
+    global group2_key
+    p = """
+    FFFFFFFF FFFFFFFF C90FDAA2 2168C234 C4C6628B 80DC1CD1
+    29024E08 8A67CC74 020BBEA6 3B139B22 514A0879 8E3404DD
+    EF9519B3 CD3A431B 302B0A6D F25F1437 4FE1356D 6D51C245
+    E485B576 625E7EC6 F44C42E9 A637ED6B 0BFF5CB6 F406B7ED
+    EE386BFB 5A899FA5 AE9F2411 7C4B1FE6 49286651 ECE65381
+    FFFFFFFF FFFFFFFF
+    """
+    q = """
+    7FFFFFFF FFFFFFFF E487ED51 10B4611A 62633145 C06E0E68
+    94812704 4533E63A 0105DF53 1D89CD91 28A5043C C71A026E
+    F7CA8CD9 E69D218D 98158536 F92F8A1B A7F09AB6 B6A8E122
+    F242DABB 312F3F63 7A262174 D31BF6B5 85FFAE5B 7A035BF6
+    F71C35FD AD44CFD2 D74F9208 BE258FF3 24943328 F67329C0
+    FFFFFFFF FFFFFFFF
+    """
+    p = int(''.join(p.split()), 16)
+    q = int(''.join(q.split()), 16)
+    g = 2
+    key = generateDSA(p, q, g)
+    validate_key(key)
+    group2_key = key
+
+
 if __name__ == '__main__':
-    print json.dumps(test_dsa_sign())
+    #print json.dumps(test_dsa_sign())
     #test_key()
+    test_group2()
